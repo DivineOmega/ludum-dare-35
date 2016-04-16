7ee29d05c43b5d73c07bc38f9c77ae3611fe1802
DivineOmega <jordan@hall05.co.uk>
2016-04-16 16:32:24 +0100

    Added question text and surrounding box

--- scenes/InterviewScene.js
+++ scenes/InterviewScene.js
@@ -3,8 +3,8 @@ function InterviewScene() {
   this.convergame = null;
 
   this.background = null;
-
   this.interviewer = null;
+  this.questionText = null;
 
   this.update = function(time) {
 
@@ -19,6 +19,10 @@ function InterviewScene() {
     this.background.render();
     this.interviewer.render();
 
+    this.convergame.draw.rectangle(100, 20, 1700, 200, '#00cc00', '#333', 15);
+
+    this.questionText.render();
+
   };
 
   this.init = function(convergame) {
@@ -27,6 +31,11 @@ function InterviewScene() {
 
     this.background = new Sprite();
     this.background.init(this.convergame, 0, 0, 1920, 1080, 'images/office/office.png');
+
+    this.questionText = new Text();
+    this.questionText.init(this.convergame, 130, 80, "Question text!");
+    this.questionText.fontSize = 40;
+    this.questionText.style = "#00CC00";
 
     this.interviewer = new Interviewer();
     this.interviewer.init(convergame);
